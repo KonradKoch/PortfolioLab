b604857f614808897316a42ecb83a8f5307bd002
Konrad <konrad.a.koch@gmail.com>
2020-07-20 00:11:17 +0200

    Dynamiczne ładowanie instytucji

--- Charity_donations/views.py
+++ Charity_donations/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Sum
+from Charity_donations.models import Donation, Institution
+
 
 # Create your views here.
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        helped_ins_num = Institution.objects
+        bags_num = Donation.objects.aggregate(sum=Sum('quantity'))['sum']
+        institution_num = helped_ins_num.count()
+        return render(request, 'index.html',
+                      {'bags': bags_num, 'institutions': institution_num})
 
 
 class AddDonation(View):
